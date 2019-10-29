310be7b5309023b6e2446b01ac24bcd7de0eb897
Sharik <shaikhsharik709@gmail.com>
2019-10-29 13:29:21 +0530

    added ability to transferwise to be able to make payouys

--- src/Drivers/TransPay/TransPay.php
+++ src/Drivers/TransPay/TransPay.php
@@ -43,4 +43,72 @@ class TransPay extends TransPayAbstract implements DriverContract
         }
         throw new \Exception('Unable to fetch exchange rates.');
     }
+
+    /**
+     * @param string $country
+     * @param array $data
+     * @return mixed
+     */
+    public function createRecipient(string $country, array $data)
+    {
+        $payload = [];
+        $payload['FirstName'] = array_splice($data['accountHolderName'], array_search(' ', $data['accountHolderName']))[0];
+//        "Receiver": {
+//            "FirstName": "Steve",
+//            "LastName": "Schulte",
+//            "MobilePhone": "123",
+//            "CountryIsoCode": "IN",
+//            "IsIndividual": "true"
+//        },
+//        "TransactionInfo": {
+//            "PaymentModeId": "C",
+//            "ReceiveCurrencyIsoCode": "INR",
+//            "SourceCurrencyIsoCode": "USD",
+//            "SentAmount": 50,
+//            "BankId":"IND10",
+//            "Account":"123456789",
+//            "BankRoutingNumber":"TESTRouting",
+//            "ReferenceNumber":"Internal7",
+//            “SourceOfFundsId":"5"
+//        }
+    }
+
+    public function createTransaction(float $amount, array $data) {
+        $userName = $this->separateFirstAndLastName($data['accountHolderName']);
+        $transactionDetails = [];
+        $transactionDetails['Receiver'] = [];
+        $transactionDetails['Receiver']['FirstName'] = $userName[0];
+        $transactionDetails['Receiver']['LastName'] = $userName[1];
+        $transactionDetails['Receiver']['MobilePhone'] = $data['phone'];
+        $transactionDetails['Receiver']['CountryIsoCode'] = $data['countryIsoCode'];
+        $transactionDetails['Receiver']['IsIndividual'] = $data['legalType'] == 'PRIVATE';
+        $transactionDetails['TransactionInfo'] = [];
+        $transactionDetails['TransactionInfo']['PaymentModeId'] = 'C';
+        $transactionDetails['TransactionInfo']['ReceiveCurrencyIsoCode'] = $data['currency'];
+        $transactionDetails['TransactionInfo']['SourceCurrencyIsoCode'] = $data['sourceCurrency'];
+        $transactionDetails['TransactionInfo']['SentAmount'] = $amount;
+        $transactionDetails['TransactionInfo']['Account'] = $data['bankDetails']['accountNumber'];
+        $transactionDetails['TransactionInfo']['BankRoutingNumber'] = $this->getRoutingNumber($data['bankDetails']);
+        $transactionDetails['TransactionInfo']['PurposeOfRemittanceId'] = 1;
+        return $this->post('/api/transaction/invoice', $transactionDetails);
+    }
+
+    public function getRoutingNumber(array $details) {
+        if (isset($details['ifscCode'])) {
+            return $details['ifscCode'];
+        }
+        if (isset($details['bankCode'])) {
+            return $details['bankCode'];
+        }
+        if (isset($details['routingNumber'])) {
+            return $details['routingNumber'];
+        }
+        if (isset($details['abartn'])) {
+            return $details['abartn'];
+        }
+    }
+
+    public function separateFirstAndLastName(string $fullName) {
+        return explode(' ', $fullName);
+    }
 }
